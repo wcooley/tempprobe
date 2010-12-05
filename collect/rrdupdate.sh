2d1f9fdf9ef7061f9babfddba13c4d01d1bb8601
Wil Cooley <wcooley@pdx.edu>
2010-12-05 02:46:09 -0800

    Renamed 'user' to 'readtemp'

--- collect/rrdupdate.sh
+++ collect/rrdupdate.sh
@@ -2,7 +2,7 @@
 
 RRD=wirebird.rrd
 
-TEMP="$(sudo ../linux/user b)"
+TEMP="$(sudo ../linux/readtemp b)"
 TEMPFMTTED=$(echo -e "$TEMP" |awk -vFS='\t' '{printf "%.2f:%.2f:%.2f\n", $1/100, $2/100, $3/100}' )
 
 rrdtool update "$RRD" "N:$TEMPFMTTED"
