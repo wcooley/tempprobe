16a82b9c29959ecb6d1572028cec7f32649b9669
Wil Cooley <wcooley@pdx.edu>
2010-12-05 02:56:12 -0800

    Update/add copyright/license & clean-up header

--- linux/usb_tempprobe.c
+++ linux/usb_tempprobe.c
@@ -1,19 +1,26 @@
 /*
- *  USB Tempprobe kernel driver
- *
- * 	
- *  
- *  Developed for Prof. Bart Massey's CS572 at Portland State University
+ * USB Tempprobe kernel driver
+ * Developed for Prof. Bart Massey's CS572 at Portland State University
  * 	Fall 2010
- *	By Freddy Carl, Wil Cooley, Indradip Ghosh & Akbar Saidov
  *
- *	This program is free software; you can redistribute it and/or
- *	modify it under the terms of the GNU General Public License as
- *	published by the Free Software Foundation, version 2.
+ * Copyright (c) 2010 by Freddy Carl, Wil Cooley and Akbar Saidov.
  *
- * 	This driver is largely based on the USB Skeleton driver - 2.0
- *  Greg Kroah-Hartman (greg@kroah.com)
- * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This driver is largely based on the USB Skeleton driver - 2.0
+ * by Greg Kroah-Hartman (greg@kroah.com) and Sean Bruno's lighty.c.
+ *
  */
 
 #include <linux/kernel.h>
--- linux/usb_tempprobe.h
+++ linux/usb_tempprobe.h
@@ -1,8 +1,32 @@
-/*usb-tempprobe.h*/
+/*
+ * usb_tempprobe.h
+ * Developed for Prof. Bart Massey's CS572 at Portland State University
+ * 	Fall 2010
+ *
+ * Copyright (c) 2010 by Freddy Carl, Wil Cooley and Akbar Saidov.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef _USB_TEMPPROBE_H
+#define _USB_TEMPPROBE_H
 
 #define TEMPPROBE_MAGIC 0xFF
-#define ENUM_CMD _IO(TEMPPROBE_MAGIC, 0)
-#define RDALL_CMD _IO(TEMPPROBE_MAGIC, 1)
+#define ENUM_CMD 	_IO(TEMPPROBE_MAGIC, 0)
+#define RDALL_CMD 	_IO(TEMPPROBE_MAGIC, 1)
 
 #define TEMPPROBE_IOCTL_MAX 1
 
+#endif /* _USB_TEMPPROBE_H */
